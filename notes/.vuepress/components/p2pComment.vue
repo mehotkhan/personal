d2f2163f53f4ad3085b864427c8366418042d224
Ali Zemani <mehot1@gmail.com>
2021-08-26 12:17:37 +0430

    Create note Article UI

---
+++ notes/.vuepress/components/p2pComment.vue
@@ -0,0 +1,219 @@
+
+<template>
+  <div>
+    <br />
+    <br />
+    <br />
+    <hr />
+    <ul uk-tab="animation: uk-animation-fade">
+      <li class="uk-active">
+        <a href="#"> دیدگاه های : {{ title }}</a>
+      </li>
+      <li>
+        <a href="#">عضویت و ارسال دیدگاه</a>
+      </li>
+    </ul>
+    <ul class="uk-switcher uk-margin">
+      <li>
+        <ul class="uk-comment-list">
+          <hr />
+          <li>
+            <article class="uk-comment uk-visible-toggle" tabindex="-1">
+              <header class="uk-comment-header uk-position-relative">
+                <div class="uk-grid-medium uk-flex-middle" uk-grid>
+                  <div class="uk-width-expand">
+                    <h4 class="uk-comment-title uk-margin-remove">
+                      <a class="uk-link-reset" href="#">نویسنده دیدگاه</a>
+                    </h4>
+                    <p class="uk-comment-meta uk-margin-remove-top">
+                      <a class="uk-link-reset" href="#">۱۲ روز پیش</a>
+                    </p>
+                  </div>
+                </div>
+                <div
+                  class="
+                    uk-position-top-right uk-position-small uk-hidden-hover
+                  "
+                >
+                  <a class="uk-link-muted" href="#">پاسخ</a>
+                </div>
+              </header>
+              <div class="uk-comment-body">
+                <p>متن دیدگاه</p>
+              </div>
+            </article>
+            <ul>
+              <li>
+                <article
+                  class="uk-comment uk-comment-primary uk-visible-toggle"
+                  tabindex="-1"
+                >
+                  <header class="uk-comment-header uk-position-relative">
+                    <div class="uk-grid-medium uk-flex-middle" uk-grid>
+                      <div class="uk-width-expand">
+                        <h4 class="uk-comment-title uk-margin-remove">
+                          <a class="uk-link-reset" href="#">نویسنده نظر </a>
+                        </h4>
+                        <p class="uk-comment-meta uk-margin-remove-top">
+                          <a class="uk-link-reset" href="#">۱۲ روز قبل</a>
+                        </p>
+                      </div>
+                    </div>
+                    <div
+                      class="
+                        uk-position-top-right uk-position-small uk-hidden-hover
+                      "
+                    >
+                      <a class="uk-link-muted" href="#">پاسخ</a>
+                    </div>
+                  </header>
+                  <div class="uk-comment-body">
+                    <p>پاسخ به دیدگاه قبل</p>
+                  </div>
+                </article>
+              </li>
+            </ul>
+          </li>
+          <hr />
+          <li>
+            <article class="uk-comment uk-visible-toggle" tabindex="-1">
+              <header class="uk-comment-header uk-position-relative">
+                <div class="uk-grid-medium uk-flex-middle" uk-grid>
+                  <div class="uk-width-expand">
+                    <h4 class="uk-comment-title uk-margin-remove">
+                      <a class="uk-link-reset" href="#">نویسنده جدید</a>
+                    </h4>
+                    <p class="uk-comment-meta uk-margin-remove-top">
+                      <a class="uk-link-reset" href="#">۱۲ روز پیش</a>
+                    </p>
+                  </div>
+                </div>
+                <div
+                  class="
+                    uk-position-top-right uk-position-small uk-hidden-hover
+                  "
+                >
+                  <a class="uk-link-muted" href="#">پاسخ</a>
+                </div>
+              </header>
+              <div class="uk-comment-body">
+                <p>متن دیدگاه</p>
+              </div>
+            </article>
+            <ul>
+              <li>
+                <article
+                  class="uk-comment uk-comment-primary uk-visible-toggle"
+                  tabindex="-1"
+                >
+                  <header class="uk-comment-header uk-position-relative">
+                    <div class="uk-grid-medium uk-flex-middle" uk-grid>
+                      <div class="uk-width-expand">
+                        <h4 class="uk-comment-title uk-margin-remove">
+                          <a class="uk-link-reset" href="#">نویسنده نظر </a>
+                        </h4>
+                        <p class="uk-comment-meta uk-margin-remove-top">
+                          <a class="uk-link-reset" href="#">۱۲ روز قبل</a>
+                        </p>
+                      </div>
+                    </div>
+                    <div
+                      class="
+                        uk-position-top-right uk-position-small uk-hidden-hover
+                      "
+                    >
+                      <a class="uk-link-muted" href="#">پاسخ</a>
+                    </div>
+                  </header>
+                  <div class="uk-comment-body">
+                    <p>پاسخ به دیدگاه قبل</p>
+                  </div>
+                </article>
+              </li>
+            </ul>
+          </li>
+          <hr />
+        </ul>
+      </li>
+      <li>
+        <form>
+          <fieldset class="uk-fieldset">
+            <div class="uk-margin">
+              <button
+                uk-icon="user"
+                class="
+                  uk-button
+                  uk-button-default
+                  uk-width-1-1
+                  uk-margin-small-bottom
+                "
+              >
+                عضویت و ورود با اثر انگشت
+              </button>
+            </div>
+            <div class="uk-margin">
+              <input disabled class="uk-input" type="نام" placeholder="نام" />
+            </div>
+
+            <div class="uk-margin">
+              <textarea
+                disabled
+                class="uk-textarea"
+                rows="5"
+                placeholder="نظر شما"
+              ></textarea>
+            </div>
+          </fieldset>
+          <p uk-margin>
+            <button
+              disabled
+              class="uk-button uk-button-primary uk-button-large"
+            >
+              ارسال دیدگاه
+            </button>
+          </p>
+        </form>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  // change this!
+  props: ["title"],
+  name: "p2pComment",
+  beforeMount() {
+    const UIkit = require("uikit");
+    const Icons = require("uikit/dist/js/uikit-icons");
+    UIkit.use(Icons);
+    window.UIkit = UIkit;
+  },
+};
+</script>
+<style src="uikit/dist/css/uikit-rtl.css"></style>
+<style>
+h1,
+.uk-h1,
+h2,
+.uk-h2,
+h3,
+.uk-h3,
+h4,
+.uk-h4,
+h5,
+.uk-h5,
+h6,
+.uk-h6,
+.uk-heading-small,
+.uk-heading-medium,
+.uk-heading-large,
+.uk-heading-xlarge,
+.uk-heading-2xlarge {
+  margin: 0 0 20px 0;
+  font-family: inherit !important;
+  font-weight: normal;
+  color: #333;
+  text-transform: none;
+}
+</style>
