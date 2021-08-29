5fbdab20e14fa71a18a4f2c24f71ba5ffd5d9702
Ali Zemani <mehot1@gmail.com>
2021-08-29 16:17:38 +0430

    update site ICONS

--- notes/.vuepress/config.js
+++ notes/.vuepress/config.js
@@ -17,20 +17,22 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ["meta", { name: "theme-color", content: "#1da1f2" }],
+    ["meta", { name: "theme-color", content: "#ffffff" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    ["link", { rel: "apple-touch-icon", href: "/icons/apple-touch-icon.png" }],
+    ["link", { rel: "icon",type:'image/png',sizes:"32x32", href: "/icons/favicon-32x32.png" }],
+    ["link", { rel: "icon",type:'image/png',sizes:"16x16", href: "/icons/favicon-16x16.png" }],
     [
       "meta",
       { name: "apple-mobile-web-app-status-bar-style", content: "black" },
     ],
     ["link", { rel: "manifest", href: "/manifest.json" }],
-    ["link", { rel: "apple-touch-icon", href: "/icons/apple-touch-icon.png" }],
     [
       "link",
       {
         rel: "mask-icon",
         href: "/icons/safari-pinned-tab.svg",
-        color: "#1da1f2",
+        color: "#5bbad5",
       },
     ],
     [
@@ -40,7 +42,7 @@ module.exports = {
         content: "/icons/msapplication-icon-144x144.png",
       },
     ],
-    ["meta", { name: "msapplication-TileColor", content: "#1da1f2" }],
+    ["meta", { name: "msapplication-TileColor", content: "#ffc40d" }],
   ],
 
   /**
