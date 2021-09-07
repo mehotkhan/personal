906d15d66675e6a0b94dee5728938c36ed237f56
Ali Zemani <mehot1@gmail.com>
2021-09-07 11:41:49 +0430

    ix Comnents UI

--- notes/.vuepress/components/p2pComment.vue
+++ notes/.vuepress/components/p2pComment.vue
@@ -16,181 +16,80 @@
       <a class="uk-alert-close" uk-close></a>
       <p>شما با موفقیت وارد شدید</p>
     </div>
-    <ul uk-tab="animation: uk-animation-fade">
-      <li>
-        <a href="#"> دیدگاه های : {{ title }}</a>
-      </li>
-      <li class="uk-active">
-        <a href="#">عضویت و ارسال دیدگاه</a>
-      </li>
-    </ul>
-    <ul class="uk-switcher uk-margin">
-      <li>
-        <ul class="uk-comment-list">
-          <li v-for="comment in commentList">
-            <article class="uk-comment uk-visible-toggle" tabindex="-1">
-              <header class="uk-comment-header uk-position-relative">
-                <div class="uk-grid-medium uk-flex-middle" uk-grid>
-                  <div class="uk-width-expand">
-                    <h4 class="uk-comment-title uk-margin-remove">
-                      <a class="uk-link-reset" href="#">نویسنده دیدگاه</a>
-                    </h4>
 
-                    <p class="uk-comment-meta uk-margin-remove-top">
-                      <a class="uk-link-reset" href="#">۱۲ روز پیش</a>
-                    </p>
-                    <p class="uk-position-bottom-right uk-hidden-hover">
-                      <a class="uk-link-reset" href="#">کلید عمومی کاربر</a>
-                      <code
-                        >Qmezm7g8mBpWyuPk6D84CNcfLKJwU6mpXuEN5GJZNkX3XK
-                      </code>
-                    </p>
-                  </div>
-                </div>
-                <div
-                  class="uk-position-top-left uk-position-small uk-hidden-hover"
-                >
-                  <a class="uk-link-muted" href="#">پاسخ به این دیدگاه</a>
-                </div>
-              </header>
-              <div class="uk-comment-body">
-                <p>{{ comment.text }}</p>
-              </div>
-            </article>
-            <ul>
-              <li>
-                <article
-                  class="uk-comment uk-comment-primary uk-visible-toggle"
-                  tabindex="-1"
-                >
-                  <header class="uk-comment-header uk-position-relative">
-                    <div class="uk-grid-medium uk-flex-middle" uk-grid>
-                      <div class="uk-width-expand">
-                        <h4 class="uk-comment-title uk-margin-remove">
-                          <a class="uk-link-reset" href="#">نویسنده نظر </a>
-                        </h4>
-                        <p class="uk-comment-meta uk-margin-remove-top">
-                          <a class="uk-link-reset" href="#">۱۲ روز قبل</a>
-                        </p>
-                        <p class="uk-position-bottom-right uk-hidden-hover">
-                          <a class="uk-link-reset" href="#">کلید عمومی کاربر</a>
-                          <code
-                            >Qmezm7g8mBpWyuPk6D84CNcfLKJwU6mpXuEN5GJZNkX3XK
-                          </code>
-                        </p>
-                      </div>
-                    </div>
-                    <div
-                      class="
-                        uk-position-top-left uk-position-small uk-hidden-hover
-                      "
-                    >
-                      <a class="uk-link-muted" href="#">پاسخ</a>
-                    </div>
-                  </header>
-                  <div class="uk-comment-body">
-                    <p>پاسخ به دیدگاه قبل</p>
-                  </div>
-                </article>
-              </li>
-            </ul>
-          </li>
-          <hr />
-        </ul>
-      </li>
-      <li>
-        <fieldset class="uk-fieldset">
-          <div class="uk-grid-small" uk-grid>
-            <div class="uk-width-1-2@s">
-              <input
-                v-model="inputData.username"
-                class="uk-input"
-                type="text"
-                placeholder="نام"
-              />
-            </div>
-            <div class="uk-width-1-2@s">
-              <input
-                v-model="inputData.emailAddress"
-                class="uk-input"
-                type="email"
-                placeholder="رایانامه"
-              />
-            </div>
-          </div>
-          <div class="uk-margin uk-grid-small" uk-grid>
-            <div class="uk-width-1-3@s">
-              <button
-                uk-icon="sign-in"
-                class="
-                  uk-button
-                  uk-button-default
-                  uk-width-1-1
-                  uk-margin-small-bottom
-                "
-                :disabled__="!(inputData.username && inputData.emailAddress)"
-                @click="register"
-              >
-                عضویت
-              </button>
-            </div>
-            <div class="uk-width-1-3@s">
-              <button
-                uk-icon="user"
-                class="
-                  uk-button
-                  uk-button-default
-                  uk-width-1-1
-                  uk-margin-small-bottom
-                "
-                :disabled__="
-                  !(inputData.username && inputData.emailAddress && savedCred)
-                "
-                @click="authenticate"
-              >
-                ورود
-              </button>
-            </div>
-            <div class="uk-width-1-3@s">
-              <button
-                uk-icon="sign-out"
-                class="
-                  uk-button
-                  uk-button-default
-                  uk-width-1-1
-                  uk-margin-small-bottom
-                "
-                :disabled__="!(assertion && savedCred)"
-                @click="exit"
-              >
-                خروج
-              </button>
-            </div>
-          </div>
-          <hr />
-          <div class="uk-margin">
-            <textarea
-              v-model="inputData.comment"
-              :disabled__="
-                !(inputData.username && inputData.emailAddress && assertion)
-              "
-              class="uk-textarea"
-              rows="5"
-              placeholder="نظر شما"
-            ></textarea>
-          </div>
-        </fieldset>
-        <p uk-margin>
-          <button
-            :disabled__="
-              !(inputData.username && inputData.emailAddress && assertion)
-            "
-            class="uk-button uk-button-primary uk-button-large"
-            @click="sendComment"
-          >
-            ارسال دیدگاه
-          </button>
-        </p>
+    <div class="uk-grid-small" uk-grid>
+      <div class="uk-width-1-2@s">
+        <input
+          v-model="inputData.username"
+          class="uk-input uk-width-1-1"
+          type="text"
+          placeholder="نام"
+        />
+      </div>
+
+      <div class="uk-width-1-4@s">
+        <button
+          uk-icon="sign-in"
+          class="uk-button uk-button-default uk-width-1-1"
+          :disabled__="!(inputData.username && inputData.emailAddress)"
+          @click="register"
+        >
+          عضویت
+        </button>
+      </div>
+      <div class="uk-width-1-4@s">
+        <button
+          uk-icon="user"
+          class="uk-button uk-button-default uk-width-1-1"
+          :disabled__="
+            !(inputData.username && inputData.emailAddress && savedCred)
+          "
+          @click="authenticate"
+        >
+          ورود
+        </button>
+      </div>
+      <div class="uk-width-1-2@s" :hidden="!(assertion && savedCred)">
+        <button
+          uk-icon="sign-out"
+          class="uk-button uk-button-default"
+          :disabled__="!(assertion && savedCred)"
+          @click="exit"
+        >
+          خروج
+        </button>
+      </div>
+    </div>
+
+    <hr />
+    <div class="uk-margin">
+      <textarea
+        v-model="inputData.comment"
+        :disabled__="
+          !(inputData.username && inputData.emailAddress && assertion)
+        "
+        class="uk-textarea"
+        rows="5"
+        placeholder="نظر شما"
+      ></textarea>
+    </div>
+    <p uk-margin>
+      <button
+        :disabled__="
+          !(inputData.username && inputData.emailAddress && assertion)
+        "
+        class="uk-button uk-button-primary uk-button-large"
+        @click="sendComment"
+      >
+        ارسال دیدگاه
+      </button>
+    </p>
+    <hr />
+    <ul class="uk-list uk-list-striped">
+      <li v-for="comment in commentList">
+        <span class="uk-text-meta uk-text-small">نظر دهنده :</span>
+        <span class="uk-text-lead uk-text-default">{{ comment.text }}</span>
+        <br />
       </li>
     </ul>
   </div>
