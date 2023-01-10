11b0d09f4e03ea800ef47c9df6361a8f841538e6
Ali Zemani <mehot1@gmail.com>
2023-01-10 22:47:21 +0330

    V3

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ pnpm clean
 pnpm generate
 # cd .output/public
 # rsync -r .output/public/* root@154.91.170.15:/var/www/ngn-new --progress
-wrangler pages publish .output/public
+proxychains wrangler pages publish .output/public
 
 # NITRO_PRESET=cloudflare yarn build
 # wrangler publish
